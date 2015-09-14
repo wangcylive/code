5e6daade786d800c04b9b8f2c4cf871e19dc9c97
wangchunyang <wangcylive@outlook.com>
2015-09-14 18:25:52 +0800

    地图定位

--- html/test/baiduMap/fn.js
+++ html/test/baiduMap/fn.js
@@ -1,42 +1,61 @@
 (function() {
 
-    function getCurrentPosition() {
-        navigator.geolocation.getCurrentPosition(success, error, options);
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            var latitude = pos.coords.latitude,
+                longitude = pos.coords.longitude;
+
+            console.log(pos);
+        }, function(err) {
+            console.log(err);
+            switch (err.code) {
+                case 1:
+                    console.log("permission denied");
+                    break;
+                case 2:
+                    console.log("position unavailable");
+                    break;
+                case 3:
+                    console.log("timeout");
+                    break;
+            }
+        }, {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 60000
+        })
+    } else {
+        console.info("geolocation not supported!");
     }
 
-    getCurrentPosition();
 
-    function success(pos) {
-        var latitude = pos.coords.latitude,
-            longitude = pos.coords.longitude;
+    function getMap(longitude, latitude) {
+        var map = new BMap.Map("baiduMap");                 // 创建Map实例
+        var point = new BMap.Point(longitude, latitude);    // 创建点坐标
+        map.centerAndZoom(point, 15);                       // 初始化地图,设置中心点坐标和地图级别。
 
-        getMap(longitude, latitude);
-    }
-
-    function error(err) {
-        console.log(err.message);
-    }
-
-    var options = {
-        enableHighAccuracy: false,
-        timeout: 5000,
-        maximumAge: 0
-    }
-
-    function getMap() {
-        var map = new BMap.Map("baiduMap");             // 创建Map实例
-        var point = new BMap.Point(113.39696321, 23.121297);    // 创建点坐标
-        map.centerAndZoom(point, 15);                   // 初始化地图,设置中心点坐标和地图级别。
-        map.addControl(new BMap.ZoomControl());         //添加地图缩放控件
         var marker = new BMap.Marker(point);
         map.addOverlay(marker);
-
-        var opts = {      
-            width : 100,     // 信息窗口宽度      
-            height: 50,     // 信息窗口高度      
-            title : "过来"  // 信息窗口标题     
-        }      
-        var infoWindow = new BMap.InfoWindow("我在这里", opts);  // 创建信息窗口对象      
-        map.openInfoWindow(infoWindow, marker.getPosition());      // 打开信息窗口
+        marker.enableDragging();
+        marker.addEventListener("click", function() {
+            console.log(marker.getPosition());
+        }, false);
     }
+
+    /*getMap(116.404, 39.915);*/
+
+    var map = new BMap.Map("baiduMap");
+    var point = new BMap.Point(116.331398, 39.897445);
+    map.centerAndZoom(point,12);
+
+    var geolocation = new BMap.Geolocation();
+    geolocation.getCurrentPosition(function(r){
+        if(this.getStatus() == BMAP_STATUS_SUCCESS){
+            var mk = new BMap.Marker(r.point);
+            map.addOverlay(mk);
+            map.panTo(r.point);
+        } else {
+            alert('failed'+this.getStatus());
+        }
+    },{enableHighAccuracy: true})
 }());
